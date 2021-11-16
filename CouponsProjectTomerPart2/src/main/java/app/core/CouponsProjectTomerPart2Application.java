39908f0e774d9fb2c3ba9a9d7e932d211f032668
Tomer <tomer1998s@gmail.com>
2021-11-16 11:29:22 +0200

    Initial commit

---
+++ CouponsProjectTomerPart2/src/main/java/app/core/CouponsProjectTomerPart2Application.java
@@ -0,0 +1,13 @@
+package app.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsProjectTomerPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponsProjectTomerPart2Application.class, args);
+	}
+
+}
