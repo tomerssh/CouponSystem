176b11211f2229974be4a7e113ea0892172db3b4
Tomer <tomer1998s@gmail.com>
2022-05-30 11:21:54 +0300

    update server

--- CouponsProjectTomerPart3A/src/main/java/app/core/filters/LoginFilter.java
+++ CouponsProjectTomerPart3A/src/main/java/app/core/filters/LoginFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 
 import app.core.utils.JwtUtil;
+import app.core.utils.JwtUtil.ClientDetails;
 
 public class LoginFilter implements Filter {
 	private JwtUtil jwtUtil;
@@ -21,22 +22,50 @@ public class LoginFilter implements Filter {
 		this.jwtUtil = jwtUtil;
 	}
 
+//	@Override
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//			throws IOException, ServletException {
+//		System.out.println("login filter before");
+//		HttpServletRequest req = (HttpServletRequest) request;
+//		String token = req.getHeader("token");
+//		System.out.println(token);
+//		try {
+//			jwtUtil.extractClient(token);
+//			chain.doFilter(request, response);
+//			System.out.println("login filter after");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			HttpServletResponse resp = (HttpServletResponse) response;
+//			resp.sendError(HttpStatus.UNAUTHORIZED.value(), "you are not logged in");
+//		}
+//	}
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("login filter before");
 		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+
 		String token = req.getHeader("token");
-		System.out.println(token);
-		try {
-			jwtUtil.extractClient(token);
+		System.out.println("===== LOGIN FILTER TOKEN: " + token);
+
+		if (token == null && req.getMethod().equals("OPTIONS")) {
+			System.out.println("this is preflight request: " + req.getMethod());
 			chain.doFilter(request, response);
-			System.out.println("login filter after");
-		} catch (Exception e) {
-			e.printStackTrace();
-			HttpServletResponse resp = (HttpServletResponse) response;
-			resp.sendError(HttpStatus.UNAUTHORIZED.value(), "you are not logged in");
+			return;
 		}
+
+		try {
+			ClientDetails clientDetails = jwtUtil.extractClient(token);
+			System.out.println("===== LOGIN FILTER: " + clientDetails);
+			chain.doFilter(request, response);
+			return;
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+			resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+			resp.sendError(HttpStatus.UNAUTHORIZED.value(), "not logged in");
+		}
+
 	}
 
 }
